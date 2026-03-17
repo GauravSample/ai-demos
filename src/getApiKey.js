3004fd38322d820e0296ec9471fbb3d1244615b3
gauravsaxena <gauravsaxena198@yahoo.com>
2026-03-17 10:44:53 -0400

    Remove old CRA src directory

--- src/getApiKey.js
+++
@@ -1,5 +0,0 @@
-// URL param ?key=sk-... takes priority, then falls back to .env
-export function getApiKey() {
-  const params = new URLSearchParams(window.location.search);
-  return params.get('key') || process.env.REACT_APP_OPENAI_KEY || '';
-}
